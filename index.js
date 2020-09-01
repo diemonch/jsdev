546098ea891385bef9d72320ec4b7109fb97340a
diemonch <52856727+diemonch@users.noreply.github.com>
2020-09-01 18:41:10 +0530

    version 1.2

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("This is Dev Panda: Welcome back after the freaking Covid lockdown, have a bear");
+});
+
+app.listen(3000, ()=>{
+console.log("Server is Up and Running");
+});
+
 function printString(string)
 {
     console.log('Hey did you say this :' + string + '?');
 }
 printString ("Hello there");
+
